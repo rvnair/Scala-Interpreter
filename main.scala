df477da635a320db691af59b8aa3d8f4f069620d
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-04-28 21:35:47 -0500

    enums

--- main.scala
+++ main.scala
@@ -1,5 +1,27 @@
 import scala.io.Source
 
+object Kind extends Enumeration {
+	val ELSE = Value("ELSE")
+	val END = Value("END")
+	val EQ = Value("EQ")
+	val EQEQ = Value("EQEQ")
+	val ID = Value("ID")
+	val IF = Value("IF")
+	val INT = Value("INT")
+	val LBRACE = Value("LBRACE")
+	val LEFT = Value("LEFT")
+	val MUL = Value("MUL")
+	val NONE = Value("NONE")
+	val PLUS = Value("PLUS")
+	val PRINT = Value("PRINT")
+	val RBRACE = Value("RBRACE")
+	val RIGHT = Value("RIGHT")
+	val SEMI = Value("SEMI")
+	val WHILE = Value("WHILE")
+	val FUN = Value("FUN")
+	val FUNPAR = Value("FUNPAR")
+}
+
 object Interpreter {
     def main(args: Array[String]): Unit = {
         Source.fromFile(args(0)).foreach{
