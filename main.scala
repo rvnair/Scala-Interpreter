b3bf687800d8374e586625c054814ba9cfc30031
KarthikVelayutham <karthik.velayutham@gmail.com>
2018-04-28 21:03:08 -0500

    input

--- main.scala
+++ main.scala
@@ -1,5 +1,10 @@
+import scala.io.Source
+
 object Interpreter {
     def main(args: Array[String]): Unit = {
+        Source.fromFile(args(0)).foreach{
+        	print
+        }
         println(666);
     }
 }
